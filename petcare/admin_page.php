970f417411ed2398311dd76bbcf267a817375a5f
Anupama K Patil <79805696+anupamakpatil@users.noreply.github.com>
2022-11-15 14:48:35 +0530

    Add files via upload

---
+++ petcare/admin_page.php
@@ -0,0 +1,103 @@
+<?php
+
+@include 'config1.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:loginshop.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+   
+<?php include 'admin_header.php'; ?>
+
+<section class="dashboard">
+
+   <h1 class="title">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+      <?php
+         $select_orders = $conn->prepare("SELECT * FROM `orders`");
+         $select_orders->execute();
+         $number_of_orders = $select_orders->rowCount();
+      ?>
+      <h3><?= $number_of_orders; ?></h3>
+      <p>orders placed</p>
+      <a href="admin_orders.php" class="btn">see orders</a>
+      </div>
+
+      <div class="box">
+      <?php
+         $select_products = $conn->prepare("SELECT * FROM `products`");
+         $select_products->execute();
+         $number_of_products = $select_products->rowCount();
+      ?>
+      <h3><?= $number_of_products; ?></h3>
+      <p>products added</p>
+      <a href="admin_products.php" class="btn">see products</a>
+      </div>
+
+      <div class="box">
+      <?php
+         $select_users = $conn->prepare("SELECT * FROM `users` WHERE user_type = ?");
+         $select_users->execute(['user']);
+         $number_of_users = $select_users->rowCount();
+      ?>
+      <h3><?= $number_of_users; ?></h3>
+      <p>total users</p>
+      <a href="admin_users.php" class="btn">view users</a>
+      </div>
+
+      <div class="box">
+      <?php
+         $select_admins = $conn->prepare("SELECT * FROM `users` WHERE user_type = ?");
+         $select_admins->execute(['admin']);
+         $number_of_admins = $select_admins->rowCount();
+      ?>
+      <h3><?= $number_of_admins; ?></h3>
+      <p>total admins</p>
+      <a href="admin_users.php" class="btn">view admin</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ petcare/home.php
@@ -0,0 +1,137 @@
+<?php
+
+@include 'config1.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:loginshop.php');
+};
+
+if(isset($_POST['add_to_wishlist'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+
+   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+   $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_wishlist_numbers->rowCount() > 0){
+      $message[] = 'already added to wishlist!';
+   }elseif($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+      $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+      $message[] = 'added to wishlist!';
+   }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+   $p_qty = $_POST['p_qty'];
+   $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+      if($check_wishlist_numbers->rowCount() > 0){
+         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $delete_wishlist->execute([$p_name, $user_id]);
+      }
+
+      $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+      $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+      $message[] = 'added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+
+<section class="home-category">
+
+   <h1 class="title">Choose your pet!</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/dog.png" alt="">
+         <h3>Dogs</h3>
+         <a href="category.php?category=dogs" class="btn">shop now</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cats.png" alt="">
+         <h3>Cats</h3>
+         <a href="category.php?category=cats" class="btn">shop now</a>
+      </div>
+
+      <div class="box">
+         <img src="images/birds.png" alt="">
+         <h3>Birds</h3>
+         <a href="category.php?category=birds" class="btn">shop now</a>
+      </div>
+
+      <div class="box">
+         <img src="images/fish.png" alt="">
+         <h3>Fish</h3>
+         <a href="category.php?category=fish" class="btn">shop now</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
